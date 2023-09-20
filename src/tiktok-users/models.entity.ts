3d191c7f3ebc3f5a71094fc63ab3d65222bd151d
cnery-ext <cnery@externo.auna.pe>
2023-09-20 16:12:14 -0500

    chore: date column in progress

--- src/tiktok-users/models.entity.ts
+++ src/tiktok-users/models.entity.ts
@@ -1,11 +1,5 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
-import {
-  Column,
-  Entity,
-  ManyToOne,
-  OneToMany,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('tiktok-users')
 @ObjectType()
@@ -19,56 +13,90 @@ export class User {
   fullName: string;
 
   @Column()
-  @Field()
+  @Field({ nullable: true })
   bio?: string;
 
   @Column()
-  @Field()
+  @Field({ nullable: true })
   image?: string;
 
   @Column()
   @Field()
   email: string;
 
-  //   @Column({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
-  //   createAt: Date;
+  @Column({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+  @Field()
+  createAt: Date;
 
   //   @Column({ type: 'timestamptz' })
   //   updatedAt: Date;
 
-  @OneToMany(() => Post, (post) => post.user)
-  @Field((type) => [Post], { nullable: true })
-  posts: Post[];
+  // @OneToMany(() => Post, (post) => post.user)
+  // @Field((type) => [Post], { nullable: true })
+  // posts: Post[];
+
+  // @OneToMany(() => Like, (like) => like.user)
+  // @Field((type) => [Like], { nullable: true })
+  // likes: Like[];
+
+  // @OneToMany(() => Comment, (comment) => comment.user)
+  // @Field((type) => [Comment], { nullable: true })
+  // comments: Comment[];
 }
 
-@Entity('tiktok-posts')
-@ObjectType()
-export class Post {
-  @PrimaryGeneratedColumn()
-  @Field((type) => Int!)
-  id: string;
+// @Entity('tiktok-posts')
+// @ObjectType()
+// export class Post {
+//   @PrimaryGeneratedColumn()
+//   @Field((type) => Int!)
+//   id: string;
 
-  @Column()
-  @Field()
-  text: string;
+//   @Column()
+//   @Field()
+//   content: string;
 
-  @Column()
-  @Field()
-  video: string;
+//   @Column()
+//   @Field()
+//   video: string;
 
-  @ManyToOne(() => User, (user) => user.posts)
-  user: User;
+//   @ManyToOne(() => User, (user) => user.posts)
+//   user: User;
 
-  @OneToMany(() => Like, (like) => like.post)
-  likes: Like[];
-}
+//   @OneToMany(() => Like, (like) => like.post)
+//   likes: Like[];
 
-@Entity('tiktok-likes')
-export class Like {
-  @PrimaryGeneratedColumn()
-  @Field((type) => Int!)
-  id: string;
+//   @OneToMany(() => Comment, (comment) => comment.post)
+//   comments: Comment[];
+// }
 
-  @ManyToOne(() => Post, (post) => post.likes)
-  post: Post;
-}
+// @Entity('tiktok-likes')
+// @ObjectType()
+// export class Like {
+//   @PrimaryGeneratedColumn()
+//   @Field((type) => Int!)
+//   id: string;
+
+//   @ManyToOne(() => Post, (post) => post.likes)
+//   post: Post;
+
+//   @ManyToOne(() => User, (user) => user.likes)
+//   user: User;
+// }
+
+// @Entity('tiktok-comments')
+// @ObjectType()
+// export class Comment {
+//   @PrimaryGeneratedColumn()
+//   @Field((type) => Int!)
+//   id: string;
+
+//   @Column()
+//   @Field()
+//   content?: string;
+
+//   @ManyToOne(() => User, (user) => user.comments)
+//   user: User;
+
+//   @ManyToOne(() => Post, (post) => post.comments)
+//   post: Post;
+// }
